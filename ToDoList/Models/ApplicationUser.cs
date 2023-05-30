7520e7c7424c381d06bb9e1cd369b131b0be4761
MarcusKyung <Kyungmj@gmail.com>
2023-05-29 20:45:37 -0700

    add initial project files

---
+++ ToDoList/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace ToDoList.Models
+{
+    public class ApplicationUser : IdentityUser //Extens Identiy's IdentityUser to our own custom class so we have additional functionality (could have used IdentityUser class as well)
+    {
+
+    }
+}
